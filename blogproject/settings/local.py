e838cf97b38449ad6e59c430ffd0580a0d02775a
gacima <31155177+gacima@users.noreply.github.com>
2020-03-12 17:25:16 +0800

    modify.

--- blogproject/settings/local.py
+++ blogproject/settings/local.py
@@ -2,7 +2,7 @@ from .common import *
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$5hc8m92!6wv$mc-+yb$3xpqnjlwb=mi*%a840u$&@5((3=9gi'
+SECRET_KEY = 'development-secret-key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
